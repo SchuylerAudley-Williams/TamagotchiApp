87e87e723fd21b0c4b64801389069686b85cff64
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-01-25 09:54:10 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
